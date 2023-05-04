c79ac4a274feb10c17c486bbd9e1ec0eac39aad8
bifurcatum <130781409+bifurcatum@users.noreply.github.com>
2023-05-04 13:58:25 +0300

    Работа над ошибками
    Исправлены №№2,3

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -1,23 +1,10 @@
-/**
- * Эту функцию трогать не нужно
- */
+
 function print(text) {
   console.log(text);
 }
 
-/**
- * Эту функцию нужно поменять так,
- * чтобы функция sayHello работала корректно
- */
 function isValid(name) {
-  let size, space, empty
-  if (name === null || name === undefined) {
-    empty = true;
-  } else {
-    size = name.length >= 4;
-    space = name.includes(" ")
-  }
-  return !!(size && !empty && !space)
+  return name !== null && name !== undefined && name.length >= 4 && !name.includes(" ");
 }
 
 function sayHello() {
--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,6 +1,6 @@
 function ucFirst(str) {
   let firstToCap
-  if (str === null || str === undefined || str === '') {
+  if (!str) {
     firstToCap = '';
   } else {
     firstToCap = str[0].toUpperCase() + str.slice(1);
