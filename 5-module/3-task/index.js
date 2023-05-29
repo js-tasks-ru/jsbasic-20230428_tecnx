6cf5ad878e8d1ac272e340388c0c6292837ab676
bifurcatum <130781409+bifurcatum@users.noreply.github.com>
2023-05-29 21:13:20 +0300

    Урок 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,39 @@
 function initCarousel() {
-  // ваш код...
+  document.querySelector(`.carousel__arrow_left`).style.display = 'none';
+  let rightButton = document.querySelector(`.carousel__arrow_right`);
+  let leftButton = document.querySelector(`.carousel__arrow_left`);
+  let slider = document.querySelector(`.carousel__inner`);
+  let counter = 0;
+  rightButton.addEventListener("click", () => {
+    counter--;
+    let sliderLength = counter * 500;
+    slider.style.transform = `translateX(${sliderLength}px)`;
+    if (counter < -2) {
+      rightButton.style.display = 'none';
+    } else {
+      rightButton.style.display = '';
+    }
+    if (counter >= 0) {
+      leftButton.style.display = 'none';
+    } else {
+      leftButton.style.display = '';
+    }
+  });
+  leftButton.addEventListener("click", () => {
+    counter++;
+    let sliderLength = counter * 500;
+    slider.style.transform = `translateX(${sliderLength}px)`;
+    if (counter < -2) {
+      rightButton.style.display = 'none';
+    } else {
+      rightButton.style.display = '';
+    }
+    if (counter >= 0) {
+      leftButton.style.display = 'none';
+    } else {
+      leftButton.style.display = '';
+    }
+  });
 }
+
+
