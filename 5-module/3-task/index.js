bad4cc67c1089f9f77245832e6503d46d59dd4db
bifurcatum <130781409+bifurcatum@users.noreply.github.com>
2023-05-30 17:12:16 +0300

    Исправлено Урок 5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -3,37 +3,31 @@ function initCarousel() {
   let rightButton = document.querySelector(`.carousel__arrow_right`);
   let leftButton = document.querySelector(`.carousel__arrow_left`);
   let slider = document.querySelector(`.carousel__inner`);
+  let step = slider.offsetWidth;
   let counter = 0;
   rightButton.addEventListener("click", () => {
     counter--;
-    let sliderLength = counter * 500;
+    let sliderLength = counter * step;
     slider.style.transform = `translateX(${sliderLength}px)`;
-    if (counter < -2) {
-      rightButton.style.display = 'none';
-    } else {
-      rightButton.style.display = '';
-    }
-    if (counter >= 0) {
-      leftButton.style.display = 'none';
-    } else {
-      leftButton.style.display = '';
-    }
+    toggleArrows(counter, rightButton, leftButton);
   });
   leftButton.addEventListener("click", () => {
     counter++;
-    let sliderLength = counter * 500;
+    let sliderLength = counter * step;
     slider.style.transform = `translateX(${sliderLength}px)`;
-    if (counter < -2) {
-      rightButton.style.display = 'none';
-    } else {
-      rightButton.style.display = '';
-    }
-    if (counter >= 0) {
-      leftButton.style.display = 'none';
-    } else {
-      leftButton.style.display = '';
-    }
+    toggleArrows(counter, rightButton, leftButton);
   });
 }
-
+function toggleArrows(counter, rightButton, leftButton) {
+  if (counter < -2) {
+    rightButton.style.display = 'none';
+  } else {
+    rightButton.style.display = '';
+  }
+  if (counter >= 0) {
+    leftButton.style.display = 'none';
+  } else {
+    leftButton.style.display = '';
+  }
+}
 
